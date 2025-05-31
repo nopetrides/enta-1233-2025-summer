965915cf3dce29c15aee3ed92ee09d1f10943d20
noahpetrides <nopetrides@gmail.com>
2025-05-30 22:57:08 -0700

    In-class projectiles

--- 1233_Template/Assets/1233_GameProgramming/StudentWork/Scripts/BulletManager.cs
+++ 1233_Template/Assets/1233_GameProgramming/StudentWork/Scripts/BulletManager.cs
@@ -1,14 +1,30 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Kobolds
 {
 	public class BulletManager : MonoBehaviour
 	{
+        [Header("External Scripts")]
 		[SerializeField] private Camera Cam;
+        [SerializeField] private KoboldInputs Inputs;
 
-		[SerializeField] private GameObject BulletPrefab;
+        [Header("Bullets")]
+		[FormerlySerializedAs("BulletPrefab")] 
+        [SerializeField] private PhysicsBullet PhysicsBulletPrefab;
 
-		[SerializeField] private KoboldInputs Inputs;
+        [Header("Raycast")]
+        
+        [SerializeField] private RaycastBullet BulletParticle;
+        [SerializeField] private LayerMask RaycastMask;
+        [SerializeField] private ShootType ShootingCalculation;
+
+        public enum ShootType
+        {
+            Raycast = 0,
+            Physics = 1,
+        }
 
 		private void Update()
 		{
@@ -18,9 +34,71 @@ namespace Kobolds
 
 		private void OnFirePressed()
 		{
-			var direction = Cam.transform.forward;
-
-			Instantiate(BulletPrefab, Cam.transform.position, Cam.transform.rotation);
+            Debug.Log("Firing projectile");
+            switch (ShootingCalculation)
+            {
+                case ShootType.Raycast:
+                    DoRaycastShot();
+                    break;
+                case ShootType.Physics:
+                    SpawnPhysicsBullet();
+                    break;
+                default:
+                    Debug.LogError("Unexpected value");
+                    break;
+            }
 		}
-	}
+
+        private void SpawnPhysicsBullet()
+        {
+            // does not call collision until physics system collides
+
+            PhysicsBullet spawnedBullet = Instantiate(PhysicsBulletPrefab, Cam.transform.position, Cam.transform.rotation);
+            spawnedBullet.Initialize(this);
+        }
+
+        private void DoRaycastShot()
+        {
+            if (Physics.Raycast(
+                    Cam.transform.position,
+                    Cam.transform.forward, 
+                    out RaycastHit hit,
+                    Mathf.Infinity, 
+                    RaycastMask))
+            {
+                Debug.Log("Raycast Hit!");
+                OnProjectileCollision(hit.point, hit.normal);
+            }
+            else
+            {
+                Debug.Log("Raycast Miss");
+            }
+        }
+
+        public void OnProjectileCollision(Vector3 position, Vector3 rotation)
+        {
+            // do stuff
+            
+            
+            SpawnParticle(position, rotation);
+        }
+
+        private void SpawnParticle(Vector3 position, Vector3 rotation)
+        {
+                
+            Instantiate(BulletParticle, position, Quaternion.Euler(rotation));
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            if (Inputs.Aim)
+                Gizmos.DrawLine(Cam.transform.position, Cam.transform.position + Cam.transform.forward * 100);
+        }
+
+        private void CleanupParticle()
+        {
+            
+        }
+    }
 }
